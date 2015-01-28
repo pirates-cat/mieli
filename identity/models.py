f23cf9e1567a25f78136a45bc44a5bd77f06f9a5
Dario Castañé <i@dario.im>
2015-01-28 22:41:50 +0000

    Setting basic organizational models.

--- identity/models.py
+++ identity/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
+class Organization(models.Model):
+    name = models.CharField(max_length=128)
+    # Magic numbers:
+    # - 63: max length for DNS label https://en.wikipedia.org/wiki/Domain_Name_System#Domain_name_syntax
+    # - 3: max number of DNS labels, as "subdomain.domain.tld"
+    # Domain aliases must be handled on HTTP server layer. Nginx recommended.
+    domain = models.CharField(max_length=63*3)
+    # Is a meta-organization?
+    meta = models.BooleanField()
 
+class Nexus(models.Model):
+    organization = models.ForeignKey(Organization)
+    name = models.CharField(max_length=128)
+    users = models.ManyToManyField(User)
